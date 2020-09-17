aff40843c402bdefa2887093b6a1c8bc193a838b
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-09-17 22:31:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
-def parrot(sound = "Squawk")
-  puts "It's annoying when parrots #{sound}!"
-  "It's annoying when parrots #{sound}!"
+def parrot(sound = "Squawk!")
+  puts "It's annoying when parrots #{sound}"
+  "It's annoying when parrots #{sound}"
 end
 
 parrot
